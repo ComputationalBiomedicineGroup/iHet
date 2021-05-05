5ba5548fa600b75eb866b9feb45f95d989eaab74
Gregor Sturm <mail@gregor-sturm.de>
2021-05-05 14:03:43 +0200

    Reexecute notebooks

--- analyses/50_myeloid_cells/_run_de_notebook.sh
+++ analyses/50_myeloid_cells/_run_de_notebook.sh
@@ -7,4 +7,4 @@ export OMP_NUM_cpus=1
 export MKL_NUM_cpus=1
 export OPENBLAS_NUM_cpus=1
 
-jupyter nbconvert myeloid_cluster.ipynb --execute --to html
+jupyter nbconvert 54_de_analysis.ipynb --execute --to html
