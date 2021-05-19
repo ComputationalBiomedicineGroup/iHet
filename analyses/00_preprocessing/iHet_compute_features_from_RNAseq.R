b665b5304220755e347e5428362fb4866c40d6c9
FFinotello <24915818+FFinotello@users.noreply.github.com>
2021-05-19 13:31:35 +0200

    Added R scripts for preprocessing of RNA-seq data
    Extraction of TF, pathway, cell-fraction features from counts/TPM using EASIER

---
+++ analyses/00_preprocessing/iHet_compute_features_from_RNAseq.R
@@ -0,0 +1,29 @@
+rm(list=ls())
+
+source("iHet_functions.R")
+
+cancertype <- "NSCLC"
+
+## NSCLC data
+
+load("../RData/NSCLC_expr_data_sel.RData")
+
+NSCLCobj.sel.easier <- getEASIERinfo(NSCLCobj.sel, 
+                                     cancertype = cancertype)
+
+save(NSCLCobj.sel.easier, 
+     file="../RData/NSCLC_easier.RData")
+
+
+## GTEx data
+
+load("../RData/GTEx_expr_data.RData")
+
+GTExobj.easier <- getEASIERinfo(GTExobj, 
+                                 cancertype = cancertype)
+
+save(GTExobj.easier, 
+     file="../RData/GTEx_easier.RData")
+
+
+
