22611671dbe1d88001dabd8f5700f8f1b8bdca2b
Gregor Sturm <mail@gregor-sturm.de>
2021-05-25 15:09:09 +0200

    MOFA analysis: Reproduce most plots

--- analyses/10_mofa/helper_functions.R
+++ analyses/10_mofa/helper_functions.R
@@ -1,35 +1,15 @@
+library(conflicted)
 library(MOFA2)
 library(reticulate)
 library(dplyr)
-library(DESeq2)
-library(stringr)
-# library(operators)
-library(gtools)
-library(matrixStats)
-library(doBy)
-library(reshape2)
-
-library(sjmisc)
+conflict_prefer("filter", "dplyr")
 library(ComplexHeatmap)
-library(Cairo)
 library(RColorBrewer)
-library(UpSetR)
-library(corrplot)
 library(ggpubr)
-library(data.table)
-# library(lares)
-library(extrafont)
-library(ggrepel)
 library(ggplot2)
-library(EnhancedVolcano)
-library(rstatix)
 library(cowplot)
-library(beeswarm)
-library(ggbeeswarm)
-library(ggExtra)
-# library(tikzDevice)
-library(heatmaply)
-library(plotly)
+library(tidyr)
+library(tibble)
 
 
 #' Bring features into 'tidy' format required by MOFA
@@ -80,11 +60,8 @@ processFeatures <- function(data, dataset_id) {
     print("Processing regression")
 
     linRegression <- function(dataframe.df) {
-      dataLM <- dataframe.df
-
       # Add the patients as a new column
-      # TODO split by `_` - there's likely an issue with the Sharma data (patient id only 2 chars)
-      dataLM$patient <- as.factor(substr(dataLM$sample, 1, 4))
+      dataLM <- dataframe.df %>% separate("sample", c("patient", NA), remove=FALSE)
 
       # Fit a linear model
       lm.out <- lm(value ~ patient, data = dataLM)
@@ -102,9 +79,8 @@ processFeatures <- function(data, dataset_id) {
     pathways.df <- linRegression(pathways.df)
   }
 
-  MOFAdata <-
-    do.call("rbind", list(cellfrac.df, pathways.df, tfs.df))
-  MOFAdata$group <- dataset_id
+  MOFAdata = bind_rows(cellfrac.df, pathways.df, tfs.df) %>%
+     mutate(group=dataset_id)
 
   return(MOFAdata)
 }
@@ -112,7 +88,8 @@ processFeatures <- function(data, dataset_id) {
 
 #' Plot heatmap
 generateHeatmap <- function(data, title, row_names = F) {
-  data <- acast(data, feature ~ sample, value.var = "value")
+  data <- pivot_wider(data, names_from = "sample", values_from="value", id_cols="feature") %>%
+    column_to_rownames("feature")
 
   if (sum(grepl("TCGA", colnames(data))) > 1) {
     patients <- NULL
@@ -168,7 +145,7 @@ generateHeatmap <- function(data, title, row_names = F) {
 }
 
 #' train the MOFA model
-runMOFA <- function(MOFAdata){
+runMOFA <- function(MOFAdata, out_file){
 
   MOFAobject <- create_mofa(MOFAdata)
 
@@ -185,10 +162,96 @@ runMOFA <- function(MOFAdata){
   # Run MOFA
   MOFAobject <- prepare_mofa(object = MOFAobject, data_options = data_opts , model_options = model_opts, training_options = train_opts)
 
-  model <- run_mofa(MOFAobject, outfile = paste0("./Models/",MOFAdata,".hdf5"), use_basilisk=FALSE)
+  model <- run_mofa(MOFAobject, outfile = out_file, use_basilisk=FALSE)
 
   set.seed(NULL)
 
   return(model)
 
+}
+
+#' Plot MOFA results
+plotResults <- function(model, title){
+  # Plot results
+
+  # Explore explained variance per view and per factor
+  ## Values
+  print(model@cache$variance_explained$r2_total)
+  print(model@cache$variance_explained$r2_per_factor)
+
+  ## Plotting
+  # Total explained variance per view
+
+  print(plot_variance_explained(model, x= "view", y ="factor", plot_total = TRUE)[[1]] + ggtitle(title))
+
+  print(plot_variance_explained(model, x= "view", y ="factor", plot_total = TRUE)[[2]] + ggtitle(title))
+
+  if (model@dimensions$G>1){
+
+    ## Plot group results
+
+    # Total explained variance per view
+    plot_variance_explained(model, x="group", y="factor", plot_total = T)[[2]] + ggtitle(title)
+
+    # Variance per feature per view
+    print(plot_variance_explained(model, x="group", y="factor")) + ggtitle(title)}
+}
+
+
+#' Make a complex heatmap showing the different factor weitghts for different datasets.
+buildHeatmap <- function(model, title){
+  col_fun = circlize::colorRamp2(seq(-1, 1, by = 2/10), rev(c("#67001F", "#B2182B", "#D6604D", "#F4A582",
+                                                              "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE",
+                                                              "#4393C3", "#2166AC", "#053061")))
+
+  setup <- function(weights){
+    # Define vectors to split the heatmap
+    row.split.vector <- c(rep("Immune cell\n Quantification",10), rep("Pathway\n Scores",14), rep("Transcription Factor Activity", nrow(weights)-24))
+
+    # Size of the each cell
+    setsize = 0.30 #cm
+    hm <- Heatmap(weights,
+                  name = "weights",
+                  rect_gp = gpar(col = "grey", lwd = 0.5),
+                  col = col_fun, border= T,
+                  cluster_rows = FALSE, cluster_columns = FALSE,
+                  show_row_names = T, show_column_names = T, row_names_side = "left",
+
+                  # Annotation
+                  # bottom_annotation  = HeatmapAnnotation(
+                  #`correlated factors` = combiFactorsFeatures,
+                  # col = col.list.Features,
+                  # simple_anno_size = unit(1, "mm"),
+                  # annotation_height = unit(3, "mm"),
+                  # annotation_name_gp = gpar(fontsize = 8, fontface = "bold"),
+                  # annotation_legend_param = list(title_gp = gpar(fontsize = 10, fontface = 2))),
+
+                  #labels_gp = gpar(fontsize = 7)),
+                  #grid_width = unit(5, "mm"),
+                  #legend_direction = "horizontal"),
+
+
+                  row_names_gp = gpar(fontsize = 10),
+                  row_split = row.split.vector,
+                  row_title_gp = gpar(fontsize =15, fontface = 2),
+
+                  column_names_gp  = gpar(fontsize = 9),
+                  column_title_gp  = gpar(fontsize =9, fontface = 2),
+                  column_title = title,
+
+                  width = unit(setsize, "cm")*ncol(weights) , height  = unit(setsize, "cm")*nrow(weights))
+
+    return(hm)
+  }
+
+  data <- cbind(do.call( "rbind",get_weights(model,  scale = T)))[,1:3]
+
+  # The signs of MOFA factors are meaningless. "Rotate" factors among
+  # data sources to make sure the sign is consistent among data sources.
+  #
+  # We want all factors to have positive correlation with CD8+ T cells,
+  # which makes the factors more intuitive to interpret.
+  hm1 <- setup(t(t(data)*sign(data["CD8+ T", ])))
+
+  return(hm1)
 }
