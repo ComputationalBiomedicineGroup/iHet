5bf00aaa7560d558bfe51b2cbc845c2962db4dfc
Gregor Sturm <mail@gregor-sturm.de>
2021-06-23 13:26:24 +0200

    RegressOut by variable

--- analyses/10_mofa/helper_functions.R
+++ analyses/10_mofa/helper_functions.R
@@ -53,31 +53,24 @@ processFeatures <- function(data, dataset_id) {
     mutate(value = log10(value * 100 + 0.001))
   pathways.df <- do_pivot(scale(data$pathway[[dataset_id]]), "Pathway scores")
   tfs.df <- do_pivot(data$tf[[dataset_id]], "Transcription factors")
+  
+  MOFAdata <- bind_rows(cellfrac.df, pathways.df, tfs.df) %>%
+    mutate(group = dataset_id)
 
   # These are datasets with multiple biospies per patient. Regressing
   # out effects related to the inter-patient variability (we want to focus
   # on the intra-patient variability).
-  # if (dataset_id %in% c("Jia2018", "Sharma2019")) {
-  #   print("Processing regression")
+  if (dataset_id %in% c("Jia2018", "Sharma2019")) {
+    print("Processing regression")
 
-  #   linRegression <- function(dataframe.df) {
-  #     # Fit a linear model
-  #     lm.out <- lm(value ~ patient, data = dataframe.df)
-  #     residuals <- lm.out[["residuals"]]
+    MOFAdata = MOFAdata %>% group_by(group, feature) %>%
+      group_modify(function(df, group) {
+        lm_out = lm(value ~ patient, data=df)
+        df$value = lm_out[["residuals"]]
+        df
+      })
+  }
 
-  #     # Assign the residuals as new values
-  #     dataframe.df$value <- residuals
-
-  #     return(dataframe.df)
-  #   }
-
-  #   cellfrac.df <- linRegression(cellfrac.df)
-  #   tfs.df <- linRegression(tfs.df)
-  #   pathways.df <- linRegression(pathways.df)
-  # }
-
-  MOFAdata <- bind_rows(cellfrac.df, pathways.df, tfs.df) %>%
-    mutate(group = dataset_id)
 
   return(MOFAdata)
 }
