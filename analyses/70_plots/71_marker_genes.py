5f41fdc226328fde33079da221213edbb8fbe475
Gregor Sturm <mail@gregor-sturm.de>
2021-08-17 14:48:34 +0200

    Add missing progeny plots

--- analyses/70_plots/71_marker_genes.py
+++ analyses/70_plots/71_marker_genes.py
@@ -52,12 +52,15 @@ adata_dorothea = dorothea.extract(adata)
 adata_dorothea.obs = adata_dorothea.obs.loc[:, []]
 
 # %%
+adata_dorothea.shape
+
+# %%
 for tf in adata_dorothea.var_names:
     with plt.rc_context({"figure.figsize": (8.5, 8), "figure.dpi": (96)}):
         sc.pl.umap(adata_dorothea, color=tf, add_outline=True, size=15, cmap="coolwarm", vmax=3, vmin=-3, save=f"_{tf.replace(':', '_')}.pdf")
 
 # %%
-adata_progeny = progeny.extract(adata)
+adata_progeny.obs = adata_progeny.obs.loc[:, []]
 
 # %%
 for pw in adata_progeny.var_names:
